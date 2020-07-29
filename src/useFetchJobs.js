c7db53d3834580370092ec1d44b3ba2bcaf9cf1e
MarcinBalejko <mbbm9@o2.pl>
2020-07-30 01:14:25 +0200

    :recycle::fire: Making request to API, cors error removed

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,10 +1,14 @@
 import { useReducer, useEffect } from "react";
+import axios from "axios";
 
 const ACTIONS = {
   MAKE_REQUEST: "make-request",
   GET_DATA: "get_data",
   ERROR: "error",
 };
+
+const BASE_URL =
+  "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -29,11 +33,17 @@ export default function useFetchJobs(params, page) {
 
   useEffect(() => {
     dispatch({ type: ACTIONS.MAKE_REQUEST });
+    axios
+      .get(BASE_URL, {
+        params: { markdown: true, page: page, ...params },
+      })
+      .then((res) => {
+        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+      })
+      .catch((e) => {
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
   }, [params, page]);
 
-  return {
-    jobs: [],
-    loading: true,
-    error: true,
-  };
+  return state;
 }
