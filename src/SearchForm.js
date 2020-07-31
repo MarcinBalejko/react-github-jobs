8382db8e2bdc90188b771c8dbeb3e9e8979d7b13
MarcinBalejko <mbbm9@o2.pl>
2020-07-31 15:58:20 +0200

    :recycle::wrench: Refactored form styling

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -3,28 +3,27 @@ import { Form, Col } from "react-bootstrap";
 
 export default function SearchForm({ params, onParamChange }) {
   return (
-    <div>
-      <Form className="mb-4">
-        <Form.Row className="align-items-end">
-          <Form.Group as={Col}>
-            <Form.Label>Description</Form.Label>
-            <Form.Control
-              onChange={onParamChange}
-              value={params.description}
-              name="description"
-              type="text"
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group as={Col}>
-            <Form.Label>Loacation</Form.Label>
-            <Form.Control
-              onChange={onParamChange}
-              value={params.location}
-              name="location"
-              type="text"
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group as={Col} xs="auto" className="ml-2"></Form.Group>
+    <Form className="mb-4">
+      <Form.Row className="align-items-end">
+        <Form.Group as={Col}>
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            onChange={onParamChange}
+            value={params.description}
+            name="description"
+            type="text"
+          />
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Location</Form.Label>
+          <Form.Control
+            onChange={onParamChange}
+            value={params.location}
+            name="location"
+            type="text"
+          />
+        </Form.Group>
+        <Form.Group as={Col} xs="auto" className="ml-2">
           <Form.Check
             onChange={onParamChange}
             value={params.full_time}
@@ -34,8 +33,8 @@ export default function SearchForm({ params, onParamChange }) {
             type="checkbox"
             className="mb-2"
           />
-        </Form.Row>
-      </Form>
-    </div>
+        </Form.Group>
+      </Form.Row>
+    </Form>
   );
 }
